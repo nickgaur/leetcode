4d7ab41da15cc9d00c6653ed6525f7178ce2010f
nick gaur <67472332+nickgaur@users.noreply.github.com>
2022-07-18 15:37:10 +0530

    Time: 5 ms (39.27%), Space: 8.9 MB (89.69%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -3,15 +3,13 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int a = m-1;
         int b = n-1;
-            for(int i=nums1.size()-1; i>=0; i--){
-                if(b < 0){
-                    break;
-                } 
+        int i = m+n-1;
+            while(b>=0){
                 if(a >= 0 && nums1[a] > nums2[b]){
-                    nums1[i]= nums1[a--];
+                    nums1[i--]= nums1[a--];
                 }
                 else{
-                    nums1[i]  = nums2[b--];
+                    nums1[i--]  = nums2[b--];
                 }
             }
     }
