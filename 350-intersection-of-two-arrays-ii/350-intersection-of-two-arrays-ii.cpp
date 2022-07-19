68f3b17835d6363170560ba373426657851401a2
nick gaur <67472332+nickgaur@users.noreply.github.com>
2022-07-19 15:09:23 +0530

    Time: 16 ms (16.38%), Space: 10.4 MB (51.75%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -2,14 +2,21 @@ class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         vector<int> res{};
-        // map<int, int> hashMap{};
+        map<int, int> hashMap{};
         for(int i=0;i<nums1.size(); i++){
-            for(int j=0; j<nums2.size(); j++){
-                if(nums1[i] == nums2[j]){
-                    res.push_back(nums1[i]);
-                    nums2[j] = -1;
-                    break;
-                }
+            auto it = hashMap.find(nums1[i]);
+            if(it == hashMap.end()){
+                hashMap[nums1[i]] = 1;
+            }
+            else{
+                hashMap[nums1[i]]++;
+            }
+        }
+         for(int i=0;i<nums2.size(); i++){
+            auto it = hashMap.find(nums2[i]);
+            if(it != hashMap.end() && hashMap[nums2[i]] != 0){
+                res.push_back(nums2[i]);
+                hashMap[nums2[i]]--;
             }
         }
         return res;
