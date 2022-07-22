771a488df694a356480bc51b3deac5e7b88b6bae
nick gaur <67472332+nickgaur@users.noreply.github.com>
2022-07-22 19:38:08 +0530

    Time: 40 ms (22.92%), Space: 8.8 MB (40.46%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -6,21 +6,14 @@ public:
         }
         map<char, int> m1;
         for(int i=0; i<magazine.length(); i++){
-            if(m1[magazine[i]]){
-                m1[magazine[i]]++;
-            }
-            else{
-                m1[magazine[i]] = 1;
-            }
+            m1[magazine[i]]++;
         }
 
         for(int i=0; i<ransomNote.length(); i++){
             if( m1[ransomNote[i]] == 0){
                 return false;
             }
-            if(m1[ransomNote[i]] > 0){
-                m1[ransomNote[i]]--;
-            }
+            m1[ransomNote[i]]--;
         }
         return true;
     }
