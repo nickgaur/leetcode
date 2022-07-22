63c2f2efef81909412a69e2f1ffc1cf42cc0a54d
nick gaur <67472332+nickgaur@users.noreply.github.com>
2022-07-22 17:39:25 +0530

    Time: 156 ms (5.02%), Space: 9.1 MB (100.00%) - LeetHub

---
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        map <char, int> hashmap;
+        for(int i=0; i<s.length(); i++){
+            auto it = hashmap.find(s[i]);
+            if(it != hashmap.end()){
+                hashmap[s[i]] = hashmap[s[i]] + 1;
+            }
+            else{
+                hashmap[s[i]] = 1;
+            }
+        }
+        for(int i=0; i<s.length(); i++){
+            if(hashmap[s[i]] == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
