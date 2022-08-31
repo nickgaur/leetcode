54fdd202c79bcbff9cf13fb58db31ab72850ad71
nick gaur <67472332+nickgaur@users.noreply.github.com>
2022-08-31 22:14:56 +0530

    Time: 67 ms (47.02%), Space: 18.7 MB (15.85%) - LeetHub

--- 1476-subrectangle-queries/1476-subrectangle-queries.cpp
+++ 1476-subrectangle-queries/1476-subrectangle-queries.cpp
@@ -17,10 +17,3 @@ public:
         return res[row][col];
     }
 };
-
-/**
- * Your SubrectangleQueries object will be instantiated and called as such:
- * SubrectangleQueries* obj = new SubrectangleQueries(rectangle);
- * obj->updateSubrectangle(row1,col1,row2,col2,newValue);
- * int param_2 = obj->getValue(row,col);
- */
