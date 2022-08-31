a22435b7c963b04aa9c4a9bf5f2e6a184638eb91
nick gaur <67472332+nickgaur@users.noreply.github.com>
2022-09-01 00:26:50 +0530

    Time: 11 ms (31.04%), Space: 8.7 MB (92.97%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.cpp
+++ 832-flipping-an-image/832-flipping-an-image.cpp
@@ -5,22 +5,12 @@ public:
         for(int i=0; i < n; i++){
             int low{0};
             int high{n-1};
-            while(low < high){
-                int temp = image[i][low];
-                image[i][low] = image[i][high];
+            while(low <= high){
+                int temp = image[i][low] ^ 1;
+                image[i][low] = image[i][high] ^ 1;
                 image[i][high] = temp;
                 low++;
                 high--;
-            }
-        }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(image[i][j] == 0){
-                    image[i][j] = 1;
-                }
-                else {
-                    image[i][j] = 0;
-                }
             }
         }
         return image;
