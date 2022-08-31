cc31c7540f7a113edb9bad30ca5cab9b820c208a
nick gaur <67472332+nickgaur@users.noreply.github.com>
2022-09-01 01:52:56 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (90.98%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,14 +1,17 @@
 class Solution {
-    public int fib(int n) {
-        int a = 0;
-        int b = 1;
-        int c = 1;
-        while(n > 1){
-                c = a + b;
-                a = b;
-                b = c;
-            n--;
+    static int solve(int n, int cache[]){
+        if(n <= 1){
+            return n;
         }
-        return (n == 0) ? 0 : c;
+        else if(cache[n] != 0){
+            return cache[n];
+        }
+        int res = solve(n-2, cache) + solve(n-1, cache);
+        cache[n] = res;
+        return res;
+    }
+    public int fib(int n) {
+        int cache[] = new int[n+1];
+        return solve(n, cache);
     }
 }
