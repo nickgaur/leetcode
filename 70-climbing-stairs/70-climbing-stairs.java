2bf346947d7ff8942d00971ae96eae36af5dcd0e
nick gaur <67472332+nickgaur@users.noreply.github.com>
2022-09-01 21:40:12 +0530

    Time: 1 ms (10.28%), Space: 41.3 MB (16.11%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,17 +1,30 @@
 class Solution {
-    static int fib(int n, int cache[]){
-        if(n == 0 || n == 1){
-            return n;
+    // MEMOIZATION
+    // static int fib(int n, int cache[]){
+    //     if(n == 0 || n == 1){
+    //         return n;
+    //     }
+    //     if(cache[n] != 0){
+    //         return cache[n];
+    //     }
+    //     int res = fib(n-1, cache) + fib(n-2, cache);
+    //     cache[n] = res;
+    //     return res;
+    // }
+    
+    // TABULATION
+    static int tabulation(int n){
+        int cache[] = new int[n+2];
+        cache[0] = 0;
+        cache[1] = 1;
+        for(int i=2; i < cache.length; i++){
+            cache[i] = cache[i-1] + cache[i-2];
         }
-        if(cache[n] != 0){
-            return cache[n];
-        }
-        int res = fib(n-1, cache) + fib(n-2, cache);
-        cache[n] = res;
-        return res;
+        return cache[n+1];
     }
     public int climbStairs(int n) {
-        int cache[] = new int[n+2];
-        return fib(n + 1, cache);
+        // int cache[] = new int[n+2];
+        // return fib(n + 1, cache);
+        return tabulation(n);
     }
 }
