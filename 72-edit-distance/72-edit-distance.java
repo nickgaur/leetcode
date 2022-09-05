b46201cbb34da5aa08af815f920d1fcd2b6b9be5
nick gaur <67472332+nickgaur@users.noreply.github.com>
2022-09-05 22:50:23 +0530

    Time: 9 ms (52.57%), Space: 45 MB (51.93%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -2,21 +2,21 @@ class Solution {
     
     static int solve(String word1, String word2, int i, int j, int cache[][]){
         int count = 0;
-        if(i == 0){
-            return j;
+        if(i == word1.length()){
+            return word2.length() - j;
         }
-        if(j == 0){
-            return i;
+        if(j == word2.length()){
+            return word1.length() - i;
         }
         if(cache[i][j] != 0){
             return cache[i][j];
         }
-        if(word1.charAt(i - 1) == word2.charAt(j - 1)){
-            return solve(word1, word2, i - 1, j - 1, cache);
+        if(word1.charAt(i) == word2.charAt(j)){
+            return solve(word1, word2, i + 1, j + 1, cache);
         }
-        int insert = solve(word1, word2, i - 1, j, cache);
-        int remove = solve(word1, word2, i, j - 1, cache);
-        int replace = solve(word1, word2, i - 1, j - 1, cache);
+        int insert = solve(word1, word2, i + 1, j, cache);
+        int remove = solve(word1, word2, i, j + 1, cache);
+        int replace = solve(word1, word2, i + 1, j + 1, cache);
         count = Math.min(Math.min(insert, remove), replace);
         cache[i][j] = count + 1;
         return cache[i][j];
@@ -27,6 +27,6 @@ class Solution {
         int m = word1.length();
         int n = word2.length();
         int cache[][] = new int[m+1][n+1];
-        return solve(word1, word2, m, n, cache);
+        return solve(word1, word2, 0, 0, cache);
     }
 }
