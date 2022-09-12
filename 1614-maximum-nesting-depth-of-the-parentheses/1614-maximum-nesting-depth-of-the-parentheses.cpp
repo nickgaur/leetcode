cc913223e18d727ace0e90905d8d9732e7c01542
nick gaur <67472332+nickgaur@users.noreply.github.com>
2022-09-12 19:59:39 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (87.46%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int opening{0};
+        int closing{0};
+        int maxDepth = 0;
+        for(int i=0; i<s.length(); i++){
+            // if(i == s.length() - 1 || (s[i] != '(' && s[i] != ')')){
+            //     maxDepth = max(opening - closing, maxDepth);
+            // }
+            if(s[i] == '('){
+                opening++;
+            }
+            else if(s[i] == ')'){
+                maxDepth = max(opening - closing, maxDepth);
+                closing++;
+            }
+        }
+        return maxDepth;
+    }
+};
