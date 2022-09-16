525d9542694ece0d87202a59689a0a82340a2286
nick gaur <67472332+nickgaur@users.noreply.github.com>
2022-09-16 18:52:13 +0530

    Time: 13 ms (17.03%), Space: 9.2 MB (54.92%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -5,15 +5,13 @@ public:
         for(int i=0; i<strs[0].length(); i++){
             char temp = strs[0][i];
             for(int j=1; j<strs.size(); j++){
-                if(strs[j][i] == temp){
-                    continue;
-                }
-                else{
+                if(strs[j][i] != temp){
                     return res;
                 }
             }
             res += temp;
         }
+        
         return res;
     }
 };
