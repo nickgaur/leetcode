6b458b027cba894bb565f7bd49fd9154a3174e17
nick gaur <67472332+nickgaur@users.noreply.github.com>
2022-09-16 22:49:10 +0530

    Time: 1 ms (87.63%), Space: 43.7 MB (60.13%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,21 +1,40 @@
 class Solution {
     public boolean search(int[] arr, int target) {
-        Arrays.sort(arr);
-        int low=0;
-        int high = arr.length - 1;
-        while(low <= high){
-            int mid = (high+low) / 2;
-            if(arr[mid] == target){
+        // Arrays.sort(arr);
+        // int low=0;
+        // int high = arr.length - 1;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == target){
                 return true;
-            }
-            
-            if(target < arr[mid]){
-                high = mid - 1;
-            }
-            else {
-                low = low + 1;
             }
         }
         return false;
+//         while(low <= high){
+//             int mid = (high+low) / 2;
+//             if(arr[mid] == target){
+//                 return true;
+//             }
+            
+//             // if left sorted
+//             if(arr[low] < arr[mid]){
+//                 if(target >= arr[low] && target < arr[mid]){
+//                     high = mid - 1;
+//                 }
+//                 else {
+//                     low = mid + 1;
+//                 }
+//             }
+            
+//             // if right sorted
+//             else {
+//                 if(target > arr[mid] && target <= arr[high]){
+//                     low = mid + 1;
+//                 }
+//                 else {
+//                     high = mid - 1;
+//                 }
+//             }
+//         }
+//         return false;
     }
 }
