1f2bcd54060e7159d547cfecdd1db5c1d7e81645
nick gaur <67472332+nickgaur@users.noreply.github.com>
2022-09-23 18:51:25 +0530

    Time: 25 ms (5.94%), Space: 9 MB (32.49%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int solve(const vector<vector<int>> &triangle, int col, int row, vector<vector<int>> &cache){
+        
+        if(row < triangle.size() && col == triangle[row].size()){
+            return INT_MAX;
+        }
+        if(row == triangle.size()){
+            return 0;
+        }
+        if(cache[row][col] != -1){
+            return cache[row][col];
+        }
+        long a = solve(triangle, col, row + 1, cache) + triangle[row][col];
+
+        long b = solve(triangle, col + 1, row + 1, cache) + triangle[row][col+1];
+
+
+        cache[row][col] = min(a,b);
+        return min(a, b);
+    }
+    
+    int minimumTotal(vector<vector<int>>& triangle) {
+        
+        if(triangle.size() == 1){
+            return triangle[0][0];
+        }
+        vector<vector<int>> cache(triangle.size(), vector<int> (triangle.size(), -1));
+        int t = solve(triangle, 0, 1, cache);
+        return triangle[0][0] + t;
+    }
+};
