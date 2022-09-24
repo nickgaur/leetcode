0d628534935b85c9b76b4c54ff43253a6252fe8a
nick gaur <67472332+nickgaur@users.noreply.github.com>
2022-09-24 22:44:41 +0530

    Time: 157 ms (17.44%), Space: 36.3 MB (18.93%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        map<int, int> mp1;
+        vector<vector<int>> result(2);
+        // vector<int> helper;
+        
+        unordered_set<int> set1;
         for(auto i: nums1){
-            mp1[i]++;
+            set1.insert(i);
         }
-        map<int, int> mp2;
+        unordered_set<int> set2;
         for(auto i: nums2){
-            mp2[i]++;
+            set2.insert(i);
         }
-        set<int> result1;
         for(auto i: nums1){
-            if(!mp2[i]){
-                result1.insert(i);
+            if(set2.find(i) == set2.end()){
+                result[0].push_back(i);
+                set2.insert(i);
             }
         }
-        set<int> result2;
         for(auto i: nums2){
-            if(!mp1[i]){
-                result2.insert(i);
+            if(set1.find(i) == set1.end()){
+                result[1].push_back(i);
+                set1.insert(i);
             }
         }
-        vector<vector<int>> result;
-        vector<int> helper;
-        for(auto i: result1){
-            helper.push_back(i);
-        }
-        result.push_back(helper);
-        helper.clear();
-        for(auto i: result2){
-            helper.push_back(i);
-        }
-        result.push_back(helper);
         return result;
     }
 };
