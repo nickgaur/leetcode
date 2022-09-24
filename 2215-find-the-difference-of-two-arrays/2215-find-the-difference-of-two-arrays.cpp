75ab310cb8dcfcf2b33fd7dbc3ed7294d7251123
nick gaur <67472332+nickgaur@users.noreply.github.com>
2022-09-24 22:51:22 +0530

    Time: 130 ms (30.49%), Space: 36.3 MB (19.77%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
         vector<vector<int>> result(2);
-        // vector<int> helper;
-        
         unordered_set<int> set1;
         for(auto i: nums1){
             set1.insert(i);
@@ -13,13 +11,13 @@ public:
             set2.insert(i);
         }
         for(auto i: nums1){
-            if(set2.find(i) == set2.end()){
+            if(!set2.count(i)){
                 result[0].push_back(i);
                 set2.insert(i);
             }
         }
         for(auto i: nums2){
-            if(set1.find(i) == set1.end()){
+            if(!set1.count(i)){
                 result[1].push_back(i);
                 set1.insert(i);
             }
