5a036acd771be7da9adf6636654cd9080e7d3473
nick gaur <67472332+nickgaur@users.noreply.github.com>
2022-09-26 20:33:18 +0530

    Time: 6 ms (48.51%), Space: 6.7 MB (67.96%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -2,12 +2,27 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         int x = 0;
-        for(int i=0; i<s.length(); i++){
-            x ^= s[i];
+        if(s.length() < t.length()){
+            int i;
+            for(i=0; i<s.length(); i++){
+                x ^= s[i];
+                x ^= t[i];
+            }
+            for(;i<t.length(); i++){
+                x ^= t[i];
+            }
         }
-        for(int i=0; i<t.length(); i++){
-            x ^= t[i];
+        else{
+            int i;
+            for(i=0; i<t.length(); i++){
+                x ^= s[i];
+                x ^= t[i];
+            }
+            for(;i<s.length(); i++){
+                x ^= s[i];
+            }
         }
+        
         return static_cast<char>(x);
     }
 };
