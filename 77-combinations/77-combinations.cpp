552b11567af3d9df799f3d3bf24d325b1637b432
nick gaur <67472332+nickgaur@users.noreply.github.com>
2022-09-27 00:04:52 +0530

    Time: 1082 ms (5.01%), Space: 165.1 MB (8.21%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    
-    vector<vector<int>> ans{};
-
-void generate(int n, int k, vector<int>& cur) {
-	if (k == 0) {
-		ans.push_back(cur);
-		return;
-	}
-	for (int i = n; i >= 1; --i) {
-		cur.push_back(i);
-		generate(i - 1, k - 1, cur);
-		cur.pop_back();
-	}
-}
-
-vector<vector<int>> combine(int n, int k) {
-	vector<int> cur{};
-	generate(n, k, cur);
-	return ans;
-}
+    vector<vector<int>> result;
+    void solve(int n, int k,int index, vector<int> temp){
+        if(temp.size() >= k){
+            result.push_back(temp);
+            return;
+        }
+        for(int i=index; i<=n; i++){
+            temp.push_back(i);
+            solve(n, k, i + 1, temp);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> temp;
+        solve(n, k, 1, temp);
+        return result;
+    }
 };
