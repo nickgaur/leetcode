2881daf45a15c18de7fb913a82cabdd6984907f3
nick gaur <67472332+nickgaur@users.noreply.github.com>
2026-07-16 23:26:14 +0530

    Time: 5 ms (80.68%), Space: 46.1 MB (17.16%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        int rev = 0;
+        int temp = x;
+        while(x > 0){
+            int lastDigit = x % 10;
+            rev = rev * 10 + lastDigit;
+            x = x/10;
+        }
+        return rev == temp;
+    }
+}
