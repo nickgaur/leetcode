d796f19ac1d7c916d3ea2dd2e9a9ddffda0db6a4
nick gaur <67472332+nickgaur@users.noreply.github.com>
2026-07-19 05:39:44 +0530

    Time: 3 ms (30.93%), Space: 119.5 MB (45.24%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -3,21 +3,19 @@ class Solution {
         int n = gas.length;
         int totalGas = 0;
         int totalCost = 0;
-        for(int i =0;i < n; i++){
-            totalGas += gas[i];
-            totalCost += cost[i];
-        }
-        if(totalGas < totalCost){
-            return -1;
-        }
         int startingPoint = 0;
         int currGas = 0;
         for (int i = 0; i < n; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
             currGas = currGas + gas[i] - cost[i];
             if(currGas < 0){
                 currGas = 0;
                 startingPoint = i + 1;
             }
+        }
+        if(totalGas < totalCost){
+            return -1;
         }
         return startingPoint;
     }
