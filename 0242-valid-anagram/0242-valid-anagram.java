29d7e29aec0e5ffb715c7248829b16f857dcefc6
nick gaur <67472332+nickgaur@users.noreply.github.com>
2026-08-25 01:30:58 +0530

    Time: 5 ms (62.25%), Space: 44 MB (95.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,12 +3,25 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
-        char charS[] = s.toCharArray();
-        char charT[] = t.toCharArray();
-        Arrays.sort(charS);
-        Arrays.sort(charT);
+        // char charS[] = s.toCharArray();
+        // char charT[] = t.toCharArray();
+        // Arrays.sort(charS);
+        // Arrays.sort(charT);
+        // for(int i = 0; i < s.length(); i++){
+        //     if(charS[i] != charT[i]){
+        //         return false;
+        //     }
+        // }
+
+
+        int arr[] = new int[256];
         for(int i = 0; i < s.length(); i++){
-            if(charS[i] != charT[i]){
+            arr[s.charAt(i)]++;
+            arr[t.charAt(i)]--;
+        }
+
+        for(int i = 0;i < s.length(); i++){
+            if(arr[s.charAt(i)] != 0){
                 return false;
             }
         }
