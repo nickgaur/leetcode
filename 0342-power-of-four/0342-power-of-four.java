ca9beee05a1822c8e3b873dd561588f633580a49
nick gaur <67472332+nickgaur@users.noreply.github.com>
2026-09-12 17:19:09 +0530

    Time: 1 ms (89.99%), Space: 42.2 MB (96.19%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,16 +1,9 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n == 1){
-            return true;
+        if(n == 0){
+            return false;
         }
-        int temp = 1;
-        // while(temp <= n){
-        //     if(temp == n){
-        //         return true;
-        //     }
-        //     temp = temp * 4;
-        // }
-        while(n > 1 && n % 4 == 0){
+        while(n % 4 == 0){
             n = n / 4;
         }
         return n == 1;
