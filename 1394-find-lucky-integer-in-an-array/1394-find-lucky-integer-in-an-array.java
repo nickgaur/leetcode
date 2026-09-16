304ec010fc68beb7ed8ebcdb55864be8666aaf2c
nick gaur <67472332+nickgaur@users.noreply.github.com>
2026-09-17 04:25:17 +0530

    Time: 1 ms (100%), Space: 44.8 MB (96.42%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.java
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.java
@@ -1,17 +1,34 @@
 class Solution {
     public int findLucky(int[] arr) {
-        HashMap<Integer, Integer> map = new HashMap<>();
         int n = arr.length;
-        for (int i = 0; i < n; i++) {
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
-        }
-        int res = -1;
-        for(int t: map.keySet()){
-            if(map.get(t) == t){
-            res = Math.max(res,t);
-            }
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for (int i = 0; i < n; i++) {
+        //     map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        // }
+        // int res = -1;
+        // for(int t: map.keySet()){
+        //     if(map.get(t) == t){
+        //     res = Math.max(res,t);
+        //     }
 
-            // System.out.println(t + " : " + map.get(t));
+        //     // System.out.println(t + " : " + map.get(t));
+        // }
+        // return res;
+        int temp[] = new int[501];
+        int res = -1;
+        for (int i = 0; i < n; i++) {
+            temp[arr[i]]++;
+            // if (temp[arr[i]] == arr[i]) {
+            //     res = Math.max(res, arr[i]);
+            // }
+            // else if(temp[arr[i]] < arr[i] || temp[arr[i]] > arr[i]){
+            //     res = -1;
+            // }
+        }
+        for(int i = 1; i <= 500; i++){
+            if(temp[i] == i){
+                res = i;
+            }
         }
         return res;
     }
