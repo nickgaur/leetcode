9ed572286906a29513b2bdac282d0e9ebdddacfc
nick gaur <67472332+nickgaur@users.noreply.github.com>
2026-09-17 03:12:29 +0530

    Time: 4 ms (30.72%), Space: 46.7 MB (13.8%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,68 +1,68 @@
 class Solution {
     public String intToRoman(int num) {
-        HashMap<Integer, Character> map = new HashMap<>();
-        map.put(1, 'I');
-        map.put(5, 'V');
-        map.put(10, 'X');
-        map.put(50, 'L');
-        map.put(100, 'C');
-        map.put(500, 'D');
-        map.put(1000, 'M');
+        String key[] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        int values[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
 
         String res = "";
-
-        while (num >= 1000) {
-            res += "M";
-            num = num - 1000;
-        }
-        while (num >= 900) {
-            res += "CM";
-            num = num - 900;
-        }
-        while (num >= 500) {
-            res += "D";
-            num = num - 500;
-        }
-        while (num >= 400) {
-            res += "CD";
-            num = num - 400;
-        }
-        while (num >= 100) {
-            res += "C";
-            num = num - 100;
-        }
-        while (num >= 90) {
-            res += "XC";
-            num = num - 90;
-        }
-        while (num >= 50) {
-            res += "L";
-            num = num - 50;
-        }
-        while (num >= 40) {
-            res += "XL";
-            num = num - 40;
-        }
-        while (num >= 10) {
-            res += "X";
-            num = num - 10;
-        }
-        while (num >= 9) {
-            res += "IX";
-            num = num - 9;
-        }
-        while (num >= 5) {
-            res += "V";
-            num = num - 5;
-        }
-        while (num >= 4) {
-            res += "IV";
-            num = num - 4;
-        }
-        while (num >= 1) {
-            res += "I";
-            num = num - 1;
+        for (int i = 0; i < key.length; i++) {
+            while (num >= values[i]) {
+                res += key[i];
+                num -= values[i];
+            }
         }
         return res;
+        // while (num >= 1000) {
+        //     res += "M";
+        //     num = num - 1000;
+        // }
+        // while (num >= 900) {
+        //     res += "CM";
+        //     num = num - 900;
+        // }
+        // while (num >= 500) {
+        //     res += "D";
+        //     num = num - 500;
+        // }
+        // while (num >= 400) {
+        //     res += "CD";
+        //     num = num - 400;
+        // }
+        // while (num >= 100) {
+        //     res += "C";
+        //     num = num - 100;
+        // }
+        // while (num >= 90) {
+        //     res += "XC";
+        //     num = num - 90;
+        // }
+        // while (num >= 50) {
+        //     res += "L";
+        //     num = num - 50;
+        // }
+        // while (num >= 40) {
+        //     res += "XL";
+        //     num = num - 40;
+        // }
+        // while (num >= 10) {
+        //     res += "X";
+        //     num = num - 10;
+        // }
+        // while (num >= 9) {
+        //     res += "IX";
+        //     num = num - 9;
+        // }
+        // while (num >= 5) {
+        //     res += "V";
+        //     num = num - 5;
+        // }
+        // while (num >= 4) {
+        //     res += "IV";
+        //     num = num - 4;
+        // }
+        // while (num >= 1) {
+        //     res += "I";
+        //     num = num - 1;
+        // }
+        // return res;
     }
 }
