96120924b6e2cb88fb734941f7d62e5ffbf35c8f
nick gaur <67472332+nickgaur@users.noreply.github.com>
2026-09-16 23:47:16 +0530

    Time: 7 ms (71.76%), Space: 49.7 MB (46.01%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,30 +1,49 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList<>();
-        String[] copiedStrs = strs.clone();
-        HashSet<String> set = new HashSet<>();
         int n = strs.length;
+        // List<List<String>> result = new ArrayList<>();
+        // String[] copiedStrs = strs.clone();
+        // HashSet<String> set = new HashSet<>();
 
-        for (int i = 0; i < copiedStrs.length; i++) {
-            char[] chars = copiedStrs[i].toCharArray();
-            Arrays.sort(chars);
-            String str = new String(chars);
-            copiedStrs[i] = str;
-            set.add(copiedStrs[i]);
-        }
-        // for(String s: copiedStrs){
-        //     System.out.println(s);
+        // for (int i = 0; i < copiedStrs.length; i++) {
+        //     char[] chars = copiedStrs[i].toCharArray();
+        //     Arrays.sort(chars);
+        //     String str = new String(chars);
+        //     copiedStrs[i] = str;
+        //     set.add(copiedStrs[i]);
         // }
-        for (String str : set) {
-            List<String> list = new ArrayList<>();
-            for (int i = 0; i < copiedStrs.length; i++) {
-                if (str.equals(copiedStrs[i])) {
-                    list.add(strs[i]);
-                }
-            }
-            result.add(list);
-        }
+        // for (String str : set) {
+        //     List<String> list = new ArrayList<>();
+        //     for (int i = 0; i < copiedStrs.length; i++) {
+        //         if (str.equals(copiedStrs[i])) {
+        //             list.add(strs[i]);
+        //         }
+        //     }
+        //     result.add(list);
+        // }
 
+        // return result;
+
+
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            char[] charArray = strs[i].toCharArray();
+            Arrays.sort(charArray);
+            String str = new String(charArray);
+            if (map.containsKey(str)) {
+                List<String> list = map.get(str);
+                list.add(strs[i]);
+                map.put(str, list);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(strs[i]);
+                map.put(str, list);
+            }
+        }
+        List<List<String>> result = new ArrayList<>();
+        for (String str : map.keySet()) {
+            result.add(map.get(str));
+        }
         return result;
     }
 }
